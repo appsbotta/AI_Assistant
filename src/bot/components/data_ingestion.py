17a0d87eb634d97befb65d6fd0c37e1a2311f001
Lokesh <appsbotta@gmail.com>
2025-03-10 21:20:00 +0530

    update data ingestion

--- src/bot/components/data_ingestion.py
+++ src/bot/components/data_ingestion.py
@@ -1,8 +1,6 @@
 import os
-import base64
 import requests
-import markdown2
-import html2text
+import json
 from bot import logger
 from dotenv import load_dotenv
 load_dotenv()
@@ -13,32 +11,11 @@ class DataIngestion:
         self.config = config
     
     def get_repos(self):
+        logger.info(f"Starting writing data from git hub repos")
         url = self.config.github_url
         headers = {"Authorization": f"token {os.getenv("TOKEN")}"}
         repos = requests.get(url, headers=headers).json()
         save_dir = self.config.save_dir
-        with open(os.path.join(save_dir,'repo.txt'),'w',encoding='utf-8') as f:
-            logger.info(f"Starting writing data from git hub repos")
-            for repo in repos:
-                OWNER = repo['owner']['login']
-                REPO = repo['name']
-                readme = f"https://api.github.com/repos/{OWNER}/{REPO}/readme"
-                headers = {"Authorization": f"token {os.getenv("TOKEN")}"}
-                response = requests.get(readme, headers=headers)
-
-                readme_content = "No Readme FIle"
-                if response.status_code == 200:
-                    data = response.json()
-                    readme_content = base64.b64decode(data["content"]).decode("utf-8")
-                    readme_content =  markdown2.markdown(readme_content, extras=["strip"])
-                    readme_content = html2text.html2text(readme_content)
-                
-                f.write(str(repo["name"]) + " -> " + str(repo["html_url"] +"\n"))
-                f.write("Description \n")
-                f.write(readme_content)
-                f.write(str("\n"))
-                f.write(str("\n"))
-                f.write("<=================================================================================================>")
-                f.write(str("\n"))
-                f.write(str("\n"))
-            logger.info(f"Completed writing data from git hub repos")
+        with open(os.path.join(save_dir,"data.json"),'w',encoding='utf-8') as f:
+            json.dump(repos,f,indent=4)
+        logger.info(f"Completed writing data from git hub repos")
