fc45c74dc3172cde7dff3b71275ad176a4b94b8b
Lokesh <appsbotta@gmail.com>
2025-03-16 22:16:39 +0530

    Ingestion Update

--- src/bot/components/data_ingestion.py
+++ src/bot/components/data_ingestion.py
@@ -15,7 +15,7 @@ class DataIngestion:
     def get_repos(self):
         logger.info(f"Starting writing data from git hub repos")
         url = self.config.github_url
-        headers = {"Authorization": f"token {os.getenv("TOKEN")}"}
+        headers = {"Authorization": f"token {os.getenv('TOKEN')}"}
         repos = requests.get(url, headers=headers).json()
         save_dir = self.config.save_dir
         with open(os.path.join(save_dir,"data.json"),'w',encoding='utf-8') as f:
