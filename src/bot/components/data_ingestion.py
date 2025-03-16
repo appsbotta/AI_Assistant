9cff5cb2acd3602f9c530f3fe9ba6d9983883346
Lokesh <appsbotta@gmail.com>
2025-03-16 23:40:26 +0530

    Token

--- src/bot/components/data_ingestion.py
+++ src/bot/components/data_ingestion.py
@@ -7,6 +7,7 @@ from bot import logger
 from dotenv import load_dotenv
 load_dotenv()
 from bot.entity.config_entity import DataIngestionConfig
+os.environ["TOKEN"] = os.getenv("TOKEN")
 
 class DataIngestion:
     def __init__(self,config:DataIngestionConfig):
@@ -15,7 +16,7 @@ class DataIngestion:
     def get_repos(self):
         logger.info(f"Starting writing data from git hub repos")
         url = self.config.github_url
-        headers = {"Authorization": f"token {os.getenv('TOKEN')}"}
+        headers = {"Authorization": f"token {os.environ["TOKEN"]}"}
         repos = requests.get(url, headers=headers).json()
         save_dir = self.config.save_dir
         with open(os.path.join(save_dir,"data.json"),'w',encoding='utf-8') as f:
